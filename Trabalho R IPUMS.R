384ac12c2c80afc86e081a912c1e8e856b357c41
leaosnatalia <31477771+leaosnatalia@users.noreply.github.com>
2017-11-23 11:02:55 -0200

    Initial commit

---
+++ Trabalho R IPUMS.R
@@ -0,0 +1,16 @@
+######################################
+####     TRABALHO PROGRAMAÇÃO     ####
+######################################
+
+rm(list = ls())
+
+library(haven) # pacote para importar dados
+library(tidyverse) # pacote para mexer nos dados
+library(janitor) # pacote para sumarizar dados
+library(formattable) # mudar valores para porcentagens
+
+setwd("C:/Users/leaos/Dropbox/DOUTORADO IESP/PROGRAMA??O/Trabalho R") 
+
+IPUMS <- read_csv2 ("ipumsi_00001.csv")
+
+names(IPUMS) 
